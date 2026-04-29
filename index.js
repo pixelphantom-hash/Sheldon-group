fad6aa3ec359679e6828892a1b28f8e0ec3427ff
Sheldon <mwangisheldon2@gmail.com>
2026-04-29 09:41:26 +0200

    Implement todo list functionality with tasks

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello Teacher !");
+let todoList = [];
+
+function addTask(task) {
+  todoList.push(task);
+  console.log("Task added:", task);
+}
+
+function showTasks() {
+  console.log("Your tasks:");
+  todoList.forEach((task, index) => {
+    console.log(index + 1 + ". " + task);
+  });
+}
+
+addTask("Finish homework");
+addTask("Study JavaScript");
+showTasks();let todoList = [];
+
+function addTask(task) {
+  todoList.push(task);
+  console.log("Task added:", task);
+}
+
+function showTasks() {
+  console.log("Your tasks:");
+  todoList.forEach((task, index) => {
+    console.log(index + 1 + ". " + task);
+  });
+}
+
+addTask("Finish homework");
+addTask("Study JavaScript");
+showTasks();
